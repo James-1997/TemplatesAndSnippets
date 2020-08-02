dd220828aafb7eb2a2a569c8c02259b88f05ac8f
Robson James <james.robson.junior@gmail.com>
2020-08-02 02:27:15 -0400

    Create Project to example of the Custom Project Template and File Template
    Signed-off-by: Robson James <james.robson.junior@gmail.com>

---
+++ Section Custom File Name/CodeStyle.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupViews];
+}
+
+- (void)setupViews {
+}
+
+@end
